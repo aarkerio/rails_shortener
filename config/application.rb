7cc42688e54818abb58873e757c38b4b4c6b9461
aarkerio <mmontoya@gmail.com>
2020-05-22 18:00:42 -0500

    heroku redis config

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module Rshorte
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-     config.active_job.queue_adapter = :resque
   end
 end
