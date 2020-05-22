10685e2ec84f6b9a16381f319250a5fba27abd2c
aarkerio <mmontoya@gmail.com>
2020-05-22 17:57:53 -0500

    heroku redis config

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,11 @@
 require 'resque/server'
 
-Resque.redis = Rails.env.production? ? $redis : "localhost:6379"
+if Rails.env.production?
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+  Resque.redis = REDIS
+else
+  Resque.redis = Redis.new(host: 'localhost', port: '6379')
+end
+
