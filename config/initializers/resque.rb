eb80fe3c951852c4cdaf4cc81adba77099d87d22
aarkerio <mmontoya@gmail.com>
2020-05-22 17:55:00 -0500

    Configuring resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,3 @@
 require 'resque/server'
 
-if Rails.env.production?
-  uri = URI.parse(ENV['REDISTOGO_URL'])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-
-  Resque.redis = REDIS
-else
-  Resque.redis = Redis.new(host: 'localhost', port: '6379')
-end
+Resque.redis = Rails.env.production? ? $redis : "localhost:6379"
